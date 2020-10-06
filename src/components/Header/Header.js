cf5e6930376dd1a5db851af78495a31f941243ce
Marcela <marcela.silva@tqi.com.br>
2020-10-06 20:55:28 -0300

    Hamburger menu behavior

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,16 +3,26 @@ import React, { Component } from "react";
 import MenuList from "./MenuList/MenuList";
 import HamburgerMenuIcon from './HamburgerMenuIcon';
 
-class Header extends Component {   
+class Header extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            showMenu: false
+        }
+    }
+
+    setShowMenu() {
+        this.setState({showMenu: !this.state.showMenu});
+    }    
 
     render() {
-
         return (
             <header className="header">
-                <div className="header__hamburger" >
+                <div className="header__hamburger" onClick={() => this.setState({ showMenu: !this.state.showMenu })}>
                     <HamburgerMenuIcon />
                 </div>
-                <nav className="header__menu" >
+                <nav className={this.state.showMenu ? 'header__menu' : 'header__menu header__menu--hide'} >
                     <MenuList />
                 </nav>
             </header>
