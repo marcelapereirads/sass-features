e6a2aaf30b66ec00d2cdc4b9ca174c766a89a8f6
Marcela <marcela.silva@tqi.com.br>
2020-10-10 02:15:32 -0300

    Fixing hamburger menu layout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import './App.css';
@@ -7,19 +7,37 @@ import Home from './screens/Home/Home';
 import Examples from './screens/Examples/Examples';
 import Features from './screens/Features/Features';
 
-function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Header />
-        <Switch>
-          <Route path="/" exact={true} component={Home} />
-          <Route path="/exemplos" component={Examples} />
-          <Route path="/funcionalidades" component={Features} />
-        </Switch>
-      </BrowserRouter>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      menuOpened: false
+    }
+
+    this.menuOpenedListener = this.menuOpenedListener.bind(this);
+  }
+
+  menuOpenedListener = (opened) => {
+    this.setState({ menuOpened: opened });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <Header menuOpened={this.menuOpenedListener} />
+          <div style={ this.state.menuOpened ? { marginTop: '100px' } : { marginTop: 0 }}>
+            <Switch>
+              <Route path="/" exact={true} component={Home} />
+              <Route path="/exemplos" component={Examples} />
+              <Route path="/funcionalidades" component={Features} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,22 +7,33 @@ class Header extends Component {
 
     constructor(props) {
         super(props);
+
         this.state = {
             showMenu: false
         }
+
+        this.emitOpenEvent = this.emitOpenEvent.bind(this);
     }
 
     setShowMenu() {
         this.setState({showMenu: !this.state.showMenu});
-    }    
+    }
+
+    emitOpenEvent() {
+        this.props.menuOpened(!this.state.showMenu);
+    }
 
     render() {
         return (
             <header className="header">
-                <div className="header__hamburger" onClick={() => this.setState({ showMenu: !this.state.showMenu })}>
+                <div className="header__hamburger" 
+                    onClick={() => {
+                        this.emitOpenEvent()
+                        this.setState({ showMenu: !this.state.showMenu });                        
+                    }}>
                     <HamburgerMenuIcon />
                 </div>
-                <nav className={this.state.showMenu ? 'header__menu' : 'header__menu header__menu--hide'} >
+                <nav className={ this.state.showMenu ? 'header__menu' : 'header__menu header__menu--hide' } >
                     <MenuList />
                 </nav>
             </header>
